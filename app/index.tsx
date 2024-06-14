31923a219b78fa38e4134ce96297875c7f27edf0
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-06-14 15:56:04 +0800

    Set up base WorkoutOverview layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text>Edit app/index.tsx to edit this screen.</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
