5a28496d0cf8e6c340d5edaae6f18ed34ff23c49
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-06-19 13:38:03 +0800

    Set up apollo client for app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,17 @@
 import { Stack } from 'expo-router';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const applloClient = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache(),
+});
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <ApolloProvider client={applloClient}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </ApolloProvider>
   );
 }
